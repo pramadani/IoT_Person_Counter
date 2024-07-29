ea57b361d9a12991fe6ce4ae0602ece7a08e4e1f
luft <araryap@gmail.com>
2024-07-29 08:26:20 +0700

    tes camera process

--- app/tes_cam.py
+++ app/tes_cam.py
@@ -1,72 +1,68 @@
+import streamlit as st
 import cv2
-import numpy as np
-from multiprocessing import Process, Manager # type: ignore
-import time
+import multiprocessing
+from PIL import Image
 from ultralytics import YOLO
+import time
 
-def predict_objects(capture, namespace, person_count):
-    model = YOLO('yolov8n.pt')
-    
-    results = model(capture)
-    
-    predictions = []
-    for result in results:
-        boxes = result.boxes
-        for box in boxes:
-            x1, y1, x2, y2 = box.xyxy.tolist()
-            conf = box.conf
-            cls = box.cls
-            predictions.append([x1, y1, x2, y2, conf, cls])
-
-    namespace.predict_result = predictions
-    person_count.value = len(predictions)
-
-def detect_objects(namespace, person_count):
-    while True:
-        if namespace.capture is not None:
-            predict_process = Process(target=predict_objects, args=(namespace.capture, namespace, person_count))
-            predict_process.start()
-            # predict_process.join()
-
-        time.sleep(0.1)
-
-def capture_and_process(namespace):
+def capture_and_draw_frame(namespace):
     cap = cv2.VideoCapture(0)
-    
+
     while True:
-        ret, capture = cap.read()
+        ret, img = cap.read()
         if not ret:
             break
+        
+        img = cv2.flip(img, 1)
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        
+        namespace.capture = Image.fromarray(img_rgb)
 
-        namespace.capture = capture.copy()
-
-        for detection in namespace.predict_result:
-            x1, y1, x2, y2, conf, cls = detection
-            cv2.rectangle(capture, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
-
-        namespace.frame = capture.copy()
-
-        cv2.imshow('Detected Objects', capture)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        if hasattr(namespace, 'result'):
+            results = namespace.result
+            for result in results:
+                for box in result.boxes:
+                    x1, y1, x2, y2 = map(int, box.xyxy[0])
+                    confidence = box.conf[0]
+                    cv2.rectangle(img_rgb, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                    cv2.putText(img_rgb, f"confidence: {confidence:.2f}", (x1, y1 - 10),
+                                cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1)
+        
+        namespace.frame = Image.fromarray(img_rgb)
 
     cap.release()
-    cv2.destroyAllWindows()
+
+def predict(namespace):
+    model = YOLO('yolov8n.pt')
+
+    while True:
+        if hasattr(namespace, 'capture'):
+            img = namespace.capture
+            results = model(img)
+            namespace.result = results
 
 def main():
-    manager = Manager()
+    manager = multiprocessing.Manager() # type: ignore
     namespace = manager.Namespace()
-    
-    namespace.capture = None
-    namespace.predict_result = []
-    namespace.frame = None
-    person_count = manager.Value('i', 0)
 
-    detection_process = Process(target=detect_objects, args=(namespace,person_count))
-    detection_process.start()
+    capture_process = multiprocessing.Process(target=capture_and_draw_frame, args=(namespace,)) # type: ignore
+    predict_process = multiprocessing.Process(target=predict, args=(namespace,)) # type: ignore
 
-    capture_process = Process(target=capture_and_process, args=(namespace,))
     capture_process.start()
+    predict_process.start()
 
-if __name__ == "__main__":
+    st.title('Live Kamera dengan YOLOv8n')
+
+    frame_ph = st.empty()
+    with frame_ph:
+        st.write("Menunggu frame...")
+
+    while True:
+        if hasattr(namespace, 'frame'):
+            frame = namespace.frame
+            with frame_ph:
+                st.image(frame, caption='Live Kamera', use_column_width=True)
+        time.sleep(0.02)
+
+if __name__ == '__main__':
     main()
