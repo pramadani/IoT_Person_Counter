3a3bec2ef9616ffd6b2133c3c571a8d993073e36
Ararya Pramadani <araryap@gmail.com>
2024-08-01 15:40:53 +0700

    fix sensor

--- app/sensor.py
+++ app/sensor.py
@@ -1,7 +1,19 @@
-import socket
+from bme280 import BME280
+from machine import I2C, Pin
 import time
-import random
-from port import IP, PORT
+import socket
+import network
+
+def connect_wifi(ssid):
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid)
+
+    while not wlan.isconnected():
+        print('Menghubungkan ke WiFi...')
+        time.sleep(1)
+    
+    print('Terhubung ke WiFi:', wlan.ifconfig())
 
 def conn_init(IP, PORT):
     addr = (IP, PORT)
@@ -16,14 +28,20 @@ def send_to_server(client_socket, data):
     client_socket.send(data.encode('utf-8'))
     print(f"Sent temperature: {data}")
 
-def get_temperature():
-    temperature = random.randint(16, 30)
+def get_temperature(sensor):
+    temperature = sensor.values[0]
     return str(temperature)
 
 if __name__ == "__main__":  
+    sensor = BME280(i2c=I2C(1, sda=Pin(6), scl=Pin(7)))
+
+    connect_wifi('BOE-')
+
+    IP = "192.168.57.140"
+    PORT = 65432
     client_socket = conn_init(IP, PORT)
     
     while True:
-        temperature = get_temperature()
+        temperature = get_temperature(sensor)
         send_to_server(client_socket, temperature)
         time.sleep(1)
