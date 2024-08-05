148bb86ba118c70acb40d0572c5dd24c0ade1eb5
Ararya Pramadani <araryap@gmail.com>
2024-08-05 13:54:38 +0700

    optimize list

--- data.py
+++ data.py
@@ -22,19 +22,20 @@ def update_temp_df(interval, temperature):
         temperature = float(temperature)
         
         temperature_data.append((current_time_str, temperature))
+
+        if len(temperature_data) > 10:
+            temperature_data = temperature_data[-10:]
         
         df = pd.DataFrame(temperature_data, columns=["Time", "Temperature"])
         df.set_index("Time", inplace=True)
-        
-        df_last_10 = df.tail(10)
                 
-        min_temp = df_last_10["Temperature"].min()
-        max_temp = df_last_10["Temperature"].max()
+        min_temp = df["Temperature"].min()
+        max_temp = df["Temperature"].max()
         y_min = min_temp - 1
         y_max = max_temp + 1
         
         fig_temp.data = []
-        fig_temp.add_trace(go.Scatter(x=df_last_10.index, y=df_last_10["Temperature"], mode='lines+markers', name='Temperature'))
+        fig_temp.add_trace(go.Scatter(x=df.index, y=df["Temperature"], mode='lines+markers', name='Temperature'))
         fig_temp.update_layout(
             title='Linimasa Temperatur',
             xaxis_title='Time',
@@ -59,19 +60,20 @@ def update_count_df(interval, person_count):
         person_count = int(person_count)
         
         person_data.append((current_time_str, person_count))
+
+        if len(person_data) > 10:
+            person_data = person_data[-10:]
         
         df = pd.DataFrame(person_data, columns=["Time", "Person Count"])
         df.set_index("Time", inplace=True)
-        
-        df_last_10 = df.tail(10)
-                
-        min_person = df_last_10["Person Count"].min()
-        max_person = df_last_10["Person Count"].max()
+                        
+        min_person = df["Person Count"].min()
+        max_person = df["Person Count"].max()
         y_min = min_person - 1
         y_max = max_person + 1
         
         fig_person.data = []
-        fig_person.add_trace(go.Scatter(x=df_last_10.index, y=df_last_10["Person Count"], mode='lines+markers', name='Person Count'))
+        fig_person.add_trace(go.Scatter(x=df.index, y=df["Person Count"], mode='lines+markers', name='Person Count'))
         fig_person.update_layout(
             title='Linimasa Person',
             xaxis_title='Time',
